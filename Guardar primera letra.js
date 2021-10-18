10b02b2f6f065243e4bbed01374feb0bc7a2547b
Daniel050403 <89996090+Daniel050403@users.noreply.github.com>
2021-10-18 10:07:36 -0500

    Update Guardar primera letra.js

--- Guardar primera letra.js
+++ Guardar primera letra.js
@@ -1,11 +1,15 @@
-var palabra= prompt("ingrese la palabra");
+
+var palabra= "ingrese la palabra";
 var letras= [],
 partir= (palabra),
 function partir(palabra) {
-    for(var x=0, x <=  palabra.length,  x + x){
+    for(var x=0;x=palabra.length;x++)
+    {
         letras.push(palabra[x])
     }
     
 }
+
+
 
 
